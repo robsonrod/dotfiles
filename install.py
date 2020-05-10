f022f4948ef1a40c6a77bb163e560a5a012f5336
Robson Rodrigues <robsonrod@gmail.com>
2020-05-10 12:38:10 -0300

    Adding plugins Gutentags and Gutentags_plus in order to pile up indexing and searching

--- install.py
+++ install.py
@@ -6,9 +6,11 @@ import os.path;
 home = os.environ['HOME']
 vimrc = f'{home}/.config/nvim/init.vim'
 bundle = f'{home}/.vim/bundle'
+tmux_conf = f'{home}/.tmux.conf'
+tmux_tpm = f'{home}/.tmux/plugins/tpm'
 work_dir = os.getcwd()
 
-os.system("sudo apt-get install git && sudo apt-get install python3 && sudo apt-get install neovim");
+os.system('sudo apt-get install git && sudo apt-get install neovim && sudo apt-get install tmux');
 
 if os.path.exists(vimrc):
     os.system(f'mv {vimrc} {vimrc}.old')
@@ -19,6 +21,16 @@ home = os.environ['HOME']
 if os.path.exists(f'{home}/.vim/bundle'):
     print('Nothing todo')    
 else:
-    os.system('git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim')
+    os.system(f'git clone https://github.com/VundleVim/Vundle.vim.git {bundle}/Vundle.vim')
 
 os.system('nvim --headless +PluginInstall +qall 2>/dev/null')
+
+if os.path.exists(tmux_conf):
+    os.system(f'mv {tmux_conf} {tmux_conf}.old')
+
+os.system(f'ln -sf {work_dir}/tmux/tmux.conf {tmux_conf}')
+
+if os.path.exists(tmux_tpm):
+    print('Nothing todo')
+else:
+    os.system(f'git clone https://github.com/tmux-plugins/tpm {tmux_tpm}')
