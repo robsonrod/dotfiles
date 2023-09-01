95a1ae4cd81cf65c2d846ae734377bde05916b30
Robson Rodrigues <robsonrod@gmail.com>
2023-09-01 18:55:01 -0300

    Update to ubuntu

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -912,17 +912,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(shfmt treemacs-all-the-icons magit org-roam visual-fill-column
-                                     org-bullets dashboard yaml-mode which-key vterm-toggle
-                                     use-package rust-mode ripgrep rainbow-delimiters perspective
-                                     paredit page-break-lines minions lsp-ui lsp-ivy ivy-rich iedit
-                                     helpful git-gutter-fringe general flycheck-clj-kondo
-                                     find-file-in-project exec-path-from-shell evil-nerd-commenter
-                                     evil-collection elisp-format doom-themes doom-modeline
-                                     dockerfile-mode dired-single dired-ranger dired-rainbow
-                                     dired-open dired-hide-dotfiles dired-collapse diminish dap-mode
-                                     counsel-projectile company-restclient company-box clang-format
-                                     cider ccls all-the-icons-dired)))
+ '(package-selected-packages
+   '(shfmt treemacs-all-the-icons magit org-roam visual-fill-column org-bullets dashboard yaml-mode which-key vterm-toggle use-package rust-mode ripgrep rainbow-delimiters perspective paredit page-break-lines minions lsp-ui lsp-ivy ivy-rich iedit helpful git-gutter-fringe general flycheck-clj-kondo find-file-in-project exec-path-from-shell evil-nerd-commenter evil-collection elisp-format doom-themes doom-modeline dockerfile-mode dired-single dired-ranger dired-rainbow dired-open dired-hide-dotfiles dired-collapse diminish dap-mode counsel-projectile company-restclient company-box clang-format cider ccls all-the-icons-dired)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
