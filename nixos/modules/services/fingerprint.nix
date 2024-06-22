8e7bd60548f9b1ff63e4f9c3b9afcabcb066c5eb
Robson Rodrigues <robsonrod@gmail.com>
2024-06-22 17:25:59 -0300

    Update finger

--- nixos/modules/services/fingerprint.nix
+++ nixos/modules/services/fingerprint.nix
@@ -4,19 +4,39 @@ with lib;
 with lib.types;
 let cfg = config.modules.services.fingerprint;
 in {
-  options.modules.services.fingerprint = {
-    enable = mkOption {
-      type = bool;
-      default = false;
-    };
-  };
-
-  config = mkIf cfg.enable {
-    security.pam.services = {
-      login.fprintAuth = true;
-      xscreensaver.fprintAuth = true;
+    options.modules.services.fingerprint = {
+        enable = mkOption {
+            type = bool;
+            default = false;
+        };
     };
 
-    services.fprintd.enable = true;
-  };
+    config = mkIf cfg.enable {
+        systemd = {
+            user.services.polkit-gnome-authentication-agent-1 = {
+                description = "polkit-gnome-authentication-agent-1";
+                wantedBy = [ "graphical-session.target" ];
+                wants = [ "graphical-session.target" ];
+                after = [ "graphical-session.target" ];
+                serviceConfig = {
+                    Type = "simple";
+                    ExecStart = "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1";
+                    Restart = "on-failure";
+                    RestartSec = 1;
+                    TimeoutStopSec = 10;
+                };
+            };
+        };
+
+        security.pam.services = {
+            betterlockscreen.fprintAuth = true;
+            login.fprintAuth = true;
+            sudo.fprintAuth = true;
+            polkit.fprintAuth = true;
+        };
+
+        services.fprintd = { 
+            enable = true;
+        };
+    };
 }
