22357af97264b01c697b56e146d1a50736671883
Robson Rodrigues <robsonrod@gmail.com>
2024-06-24 15:09:27 -0300

    Add camera support

--- nixos/hosts/nixos-test/hardware-configuration.nix
+++ nixos/hosts/nixos-test/hardware-configuration.nix
@@ -3,15 +3,42 @@
 # to /etc/nixos/configuration.nix instead.
 { config, lib, pkgs, modulesPath, ... }:
 
+let
+    ivsc-firmware = with pkgs;
+    stdenv.mkDerivation rec {
+      pname = "ivsc-firmware";
+      version = "main";
+
+      src = pkgs.fetchFromGitHub {
+        owner = "intel";
+        repo = "ivsc-firmware";
+        rev = "10c214fea5560060d387fbd2fb8a1af329cb6232";
+        sha256 = "sha256-kEoA0yeGXuuB+jlMIhNm+SBljH+Ru7zt3PzGb+EPBPw=";
+
+      };
+
+      installPhase = ''
+        mkdir -p $out/lib/firmware/vsc/soc_a1_prod
+
+        cp firmware/ivsc_pkg_ovti01a0_0.bin $out/lib/firmware/vsc/soc_a1_prod/ivsc_pkg_ovti01a0_0_a1_prod.bin
+        cp firmware/ivsc_skucfg_ovti01a0_0_1.bin $out/lib/firmware/vsc/soc_a1_prod/ivsc_skucfg_ovti01a0_0_1_a1_prod.bin
+        cp firmware/ivsc_fw.bin $out/lib/firmware/vsc/soc_a1_prod/ivsc_fw_a1_prod.bin
+      '';
+    };
+in 
+
 {
   imports =
     [ (modulesPath + "/installer/scan/not-detected.nix")
     ];
 
-  boot.initrd.availableKernelModules = [ "xhci_pci" "thunderbolt" "vmd" "nvme" "usb_storage" "sd_mod" ];
+  boot.initrd.availableKernelModules = [ "xhci_pci" "thunderbolt" "vmd" "nvme" "usb_storage" "usbhid" "sd_mod" "i915" ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-intel" ];
   boot.extraModulePackages = [ ];
+  boot.kernelParams = [
+    "i915.enable_psr=0"
+  ];
 
   fileSystems."/" =
     { device = "/dev/disk/by-uuid/0ec87f4f-5398-42e9-9e4c-fe6650b669af";
@@ -42,4 +69,15 @@
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   powerManagement.cpuFreqGovernor = lib.mkDefault "powersave";
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+
+  hardware.ipu6 = {
+      enable = true;
+      platform = "ipu6ep";
+  };
+
+  hardware.enableRedistributableFirmware = true;
+
+  hardware.firmware = [
+    ivsc-firmware
+  ];
 }
