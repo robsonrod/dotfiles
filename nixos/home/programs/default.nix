d8e264fdd53eca8294c573cb155bcd630ce00ee7
Robson Rodrigues <robsonrod@gmail.com>
2024-06-27 15:45:48 -0300

    Update Nixos config

--- nixos/home/programs/default.nix
+++ nixos/home/programs/default.nix
@@ -4,13 +4,10 @@
   imports = [
     ./browsers.nix
     ./cli.nix
-    ./clojure.nix
     ./fish.nix
     ./git.nix
     ./network-manager.nix
-    ./rust.nix
     ./terms.nix
-    ./python.nix
     ./pdfeditor.nix
     ./neovim.nix
   ];
