4f054a6e230138fcc188e2900cf6ea5e7de32822
Robson Rodrigues <robsonrod@gmail.com>
2025-01-12 14:27:33 -0300

    Add screenshot

--- emacs/.config/emacs/init.el
+++ emacs/.config/emacs/init.el
@@ -1180,7 +1180,12 @@ The DWIM behaviour of this command is as follows:
   :defer t
   :ensure t)
 
+(use-package
+  transient
+  :defer t)
+
 (require 'screenshot)
+
 
 (defun remacs/pdf-midnight ()
   "Set pdf-view-midnight colors"
