6cd30e396e673bb87f3e2e8e3c46ebbc94931c5c
Robson Rodrigues <robsonrod@gmail.com>
2025-01-14 18:30:11 -0300

    Update EDITOR and VISUAL variables

--- bash/.config/bash/variables/apps.sh
+++ bash/.config/bash/variables/apps.sh
@@ -2,7 +2,15 @@ export OPENER='xdg-open'
 export GPG_TTY=$(tty)
 export TERM='xterm-256color'
 export COLORTERM=truecolor
-export EDITOR="nvim"
+
+if pgrep -x emacs >/dev/null; then
+	export VISUAL="emacsclient -c"
+	export EDITOR="emacsclient -t"
+else
+	export VISUAL="nvim"
+	export EDITOR=$VISUAL
+fi
+
 export MANPAGER='less'
 export PAGER='less -s -M +Gg'
 export BROWSER='firefox'
@@ -10,13 +18,12 @@ export FILE='thunar'
 export TERMINAL='kitty'
 export LSP_USE_PLISTS=true
 
-if [ "$TERM" = "dumb" ] && [ "$INSIDE_EMACS" ] || [ "$TERM" = "dumb-emacs-ansi" ] && [ "$INSIDE_EMACS" ]
-then
-    PAGER="cat"
-    alias less="cat"
-    TERM=dumb-emacs-ansi
-    COLORTERM=1
+if [ "$TERM" = "dumb" ] && [ "$INSIDE_EMACS" ] || [ "$TERM" = "dumb-emacs-ansi" ] && [ "$INSIDE_EMACS" ]; then
+	PAGER="cat"
+	alias less="cat"
+	TERM=dumb-emacs-ansi
+	COLORTERM=1
 
-    EDITOR="emacsclient -a emacs -t -r"
-    VISUAL="emacsclient -a emacs -t -r"
+	EDITOR="emacsclient -a emacs -t -r"
+	VISUAL="emacsclient -a emacs -t -r"
 fi
